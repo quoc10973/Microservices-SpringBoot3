e0541c70bb2ec7d7c42822545031e8efae53a6a2
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2024-12-12 23:33:25 +0700

    port environment variable

--- src/main/java/com/quoc/microservices/order/client/InventoryClient.java
+++ src/main/java/com/quoc/microservices/order/client/InventoryClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "inventory-service", url = "http://localhost:8082") // Feign client for call Inventory service
+@FeignClient(value = "inventory-service", url = "${inventory.url}") // Feign client for call Inventory service
 public interface InventoryClient {
 
     // Call Inventory service to check if a product is in stock using GET method
