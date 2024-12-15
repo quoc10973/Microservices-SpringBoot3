5068ae570bbc7b5f284b52aac1932202840c52f6
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2024-12-15 23:37:54 +0700

    Filter chain + Auth White List

--- src/main/java/com/quoc/microservices/gateway/config/SecurityConfig.java
+++ src/main/java/com/quoc/microservices/gateway/config/SecurityConfig.java
@@ -9,13 +9,25 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+    private final String[] AUTH_WHITELIST = {
+            "/v3/api-docs/**",
+            "/swagger-ui/**",
+            "/swagger-ui.html",
+            "/swagger-resources/**",
+            "/api/docs/**",
+            "/aggregate/**",
+            "/api-docs/**",
+    };
+
     @Bean
     // Configure security filter chain, any HTTP request must be authenticated, JWT token is used for authentication
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.authorizeHttpRequests(authorize -> authorize.anyRequest()
+        return httpSecurity.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(AUTH_WHITELIST)
+                        .permitAll()
+                        .anyRequest()
                         .authenticated())
                         .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                         .build();
-
     }
 }
