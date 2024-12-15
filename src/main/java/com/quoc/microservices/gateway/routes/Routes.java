9c5fee5a13a16d4307a218e6b81e86549bbaf32d
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2024-12-15 23:37:07 +0700

    aggregate document openAPI for all service

--- src/main/java/com/quoc/microservices/gateway/routes/Routes.java
+++ src/main/java/com/quoc/microservices/gateway/routes/Routes.java
@@ -8,6 +8,8 @@ import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
+import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
+
 @Configuration
 public class Routes {
 
@@ -19,7 +21,17 @@ public class Routes {
                 .route("product_service")
                 .route(RequestPredicates.path("/api/products"), HandlerFunctions.http("http://localhost:8080"))
                 .build();
+    }
 
+    @Bean
+    public RouterFunction<ServerResponse> productServiceSwaggerRoute() {
+        return GatewayRouterFunctions
+                .route("product_service_swagger")
+                .route(RequestPredicates.path("/aggregate/products-service/v3/api-docs"), HandlerFunctions.http("http://localhost:8080"))
+                .filter(setPath("/v3/api-docs"))
+                .build();
+        // filter(setPath("api-docs")) set the url path http://localhost:8080/aggregate/product-service/v3/api-docs to http://localhost:8080/v3/api-docs
+        // this must be the same as the api doc path in the OpenAPIConfig.java file in the product service: ** springdoc.api-docs.path=/v3/api-docs **
     }
 
     @Bean
@@ -30,7 +42,15 @@ public class Routes {
                 .route("orders_service")
                 .route(RequestPredicates.path("/api/orders"), HandlerFunctions.http("http://localhost:8081"))
                 .build();
+    }
 
+    @Bean
+    public RouterFunction<ServerResponse> orderServiceSwaggerRoute() {
+        return GatewayRouterFunctions
+                .route("orders_service_swagger")
+                .route(RequestPredicates.path("/aggregate/orders-service/v3/api-docs"), HandlerFunctions.http("http://localhost:8081"))
+                .filter(setPath("/v3/api-docs"))
+                .build();
     }
 
     @Bean
@@ -41,6 +61,14 @@ public class Routes {
                 .route("inventory_service")
                 .route(RequestPredicates.path("/api/inventory"), HandlerFunctions.http("http://localhost:8082"))
                 .build();
+    }
 
+    @Bean
+    public RouterFunction<ServerResponse> inventoryServiceSwaggerRoute() {
+        return GatewayRouterFunctions
+                .route("inventory_service_swagger")
+                .route(RequestPredicates.path("/aggregate/inventory-service/v3/api-docs"), HandlerFunctions.http("http://localhost:8082"))
+                .filter(setPath("/v3/api-docs"))
+                .build();
     }
 }
