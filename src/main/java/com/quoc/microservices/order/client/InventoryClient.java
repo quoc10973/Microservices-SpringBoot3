93d8c5c94b12ca190ef62f0f55ae351991e14657
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2024-12-16 23:15:48 +0700

    circuit breaker with Resilience4J

--- src/main/java/com/quoc/microservices/order/client/InventoryClient.java
+++ src/main/java/com/quoc/microservices/order/client/InventoryClient.java
@@ -1,11 +1,26 @@
 package com.quoc.microservices.order.client;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 
+
 public interface InventoryClient {
+
+    Logger log = LoggerFactory.getLogger(InventoryClient.class);
 
     // Call Inventory service to check if a product is in stock using GET method
     @GetExchange("/api/inventory")
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod") // name should be the same as the name in application.properties
+    @Retry(name = "inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
+
+    // Fallback method if the Inventory service is not available
+    default boolean fallbackMethod(String skuCode, Integer quantity, Throwable throwable) {
+        log.info("Can't not get response from Inventory service for skuCode: {} failure reason: {}", skuCode, throwable.getMessage());
+        return false;
+    }
 }
--- src/main/java/com/quoc/microservices/order/config/RestClientConfig.java
+++ src/main/java/com/quoc/microservices/order/config/RestClientConfig.java
@@ -4,9 +4,13 @@ import com.quoc.microservices.order.client.InventoryClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
@@ -18,10 +22,18 @@ public class RestClientConfig {
     public InventoryClient inventoryClient() {
         // Create a REST client to call the Inventory service
         RestClient restClient = RestClient.builder().
-                baseUrl(inventoryServiceUrl).
-                build();
+                baseUrl(inventoryServiceUrl)
+                .requestFactory(getClientRequestFactory()) //
+                .build();
         var restClientAdapter = RestClientAdapter.create(restClient); // Create a REST client adapter for HttpServiceProxyFactory understand and use
         var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build(); // build the HttpServiceProxyFactory with the REST client adapter
         return httpServiceProxyFactory.createClient(InventoryClient.class); // Proxy is stand for interface InventoryClient and map it become HTTP request to call the Inventory service
+    }
+
+    private ClientHttpRequestFactory getClientRequestFactory() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout((int) Duration.ofSeconds(3).toMillis()); // Set the connection timeout to 3 seconds
+        factory.setReadTimeout((int) Duration.ofSeconds(3).toMillis()); // Set the read timeout to 3 seconds
+        return factory;
     }
 }
