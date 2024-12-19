0143252177682973489ac8cecde402d72c4b038d
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2024-12-20 00:33:41 +0700

    schema registry with Avro

--- src/main/java/com/quoc/microservices/service/NotificationService.java
+++ src/main/java/com/quoc/microservices/service/NotificationService.java
@@ -1,6 +1,6 @@
 package com.quoc.microservices.service;
 
-import com.quoc.microservices.order.events.OrderPlaceEvent;
+import com.quoc.microservices.order.event.OrderPlacedEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -18,22 +18,25 @@ public class NotificationService {
     private final JavaMailSender javaMailSender;
 
     @KafkaListener(topics = "order-placed")
-    public void listenOrderPlaced(OrderPlaceEvent orderPlaceEvent) {
-        log.info("Got message from order-placed topic : {}", orderPlaceEvent);
+    public void listenOrderPlaced(OrderPlacedEvent orderPlacedEvent) {
+        log.info("Got message from order-placed topic : {}", orderPlacedEvent);
         // Send email to customer
         MimeMessagePreparator mimeMessagePreparator = mimeMessage -> {
             MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
             messageHelper.setFrom("microservices@email.com");
-            messageHelper.setTo(orderPlaceEvent.getEmail());
-            messageHelper.setSubject(String.format("Order %s has been placed!!", orderPlaceEvent.getOrderNumber()));
+            messageHelper.setTo(orderPlacedEvent.getEmail().toString());
+            messageHelper.setSubject(String.format("Order %s has been placed!!", orderPlacedEvent.getOrderNumber()));
             messageHelper.setText(String.format(("""
-                    Hi,
+                    Hi %s %s,
                     Your order with orderNumber %s has been placed successfully.
                     
                     Thanks,
                     Microservices Team
                     """),
-                    orderPlaceEvent.getOrderNumber()));
+
+                    orderPlacedEvent.getFirstName(),
+                    orderPlacedEvent.getLastName(),
+                    orderPlacedEvent.getOrderNumber()));
         };
         try{
             javaMailSender.send(mimeMessagePreparator);
