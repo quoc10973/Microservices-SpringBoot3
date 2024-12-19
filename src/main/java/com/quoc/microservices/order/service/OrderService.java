f6f99553904d61e194ea374cb30a82bece1e1ee8
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2024-12-20 00:34:56 +0700

    schema registry with Avro

--- src/main/java/com/quoc/microservices/order/service/OrderService.java
+++ src/main/java/com/quoc/microservices/order/service/OrderService.java
@@ -2,7 +2,7 @@ package com.quoc.microservices.order.service;
 
 import com.quoc.microservices.order.client.InventoryClient;
 import com.quoc.microservices.order.entity.Order;
-import com.quoc.microservices.order.events.OrderPlaceEvent;
+import com.quoc.microservices.order.event.OrderPlacedEvent;
 import com.quoc.microservices.order.model.OrderRequest;
 import com.quoc.microservices.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
@@ -19,7 +19,7 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
-    private final KafkaTemplate<String, OrderPlaceEvent> kafkaTemplate; // Inject KafkaTemplate, generic type is <String, OrderPlaceEvent>
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate; // Inject KafkaTemplate, generic type is <String, OrderPlaceEvent>
 
     public void placeOrder(OrderRequest orderRequest) {
         // Check if product is in stock
@@ -36,14 +36,19 @@ public class OrderService {
         // Save order
         orderRepository.save(order);
         // Send message to Kafka topic
-        OrderPlaceEvent orderPlaceEvent = new OrderPlaceEvent(order.getOrderNumber(), orderRequest.userDetail().email());
+        OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent(); // mvn clean compile to generate OrderPlacedEvent class
+        orderPlacedEvent.setOrderNumber(order.getOrderNumber());
+        orderPlacedEvent.setEmail(orderRequest.userDetail().email());
+        orderPlacedEvent.setFirstName(orderRequest.userDetail().firstName());
+        orderPlacedEvent.setLastName(orderRequest.userDetail().lastName());
 
-        log.info("Start - Sending OrderPlaceEvent {} to Kafka topic order-placed", orderPlaceEvent); //log start
+        log.info("Start - Sending OrderPlaceEvent {} to Kafka topic order-placed", orderPlacedEvent); //log start
 
-        kafkaTemplate.send("order-placed", orderPlaceEvent); // Send message to Kafka topic
+        kafkaTemplate.send("order-placed", orderPlacedEvent); // Send message to Kafka topic
 
-        log.info("End - Sending OrderPlaceEvent {} to Kafka topic order-placed", orderPlaceEvent); //log end
+        log.info("End - Sending OrderPlaceEvent {} to Kafka topic order-placed", orderPlacedEvent); //log end
 
         //now can go to Kafka UI to check if the message is sent at http://localhost:8086/
+        // NOTE: If use Schema Registry by Avro, change the deserializer in application.properties to io.confluent.kafka.serializers.KafkaAvroDeserializer
     }
 }
